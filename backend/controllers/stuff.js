ff53f3da2a1647f6f7205f6b96127b2e39681016
Sam-LP <rubikscube100x100@gmail.com>
2025-09-30 19:07:36 +0200

    config prettier + removed semis

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -2,38 +2,52 @@
 import Thing from "../models/Thing.js"
 
 const thingController = {
-    getThings : async (req, res) => {
+  // GET (all)
+  getThings: async (req, res) => {
+    const things = await Thing.find()
+    res.status(200).json(things)
+  },
+  // GET (by id)
+  getOneThing: async (req, res) => {
+    const thing = await Thing.findById(req.params.id)
 
-      const things = await Thing.find()
-      res.status(200).json(things)
-      
-    },
-    createThing : async (req, res) => {
-        // delete req.body._id; // pour retirer le champ "id" de la requête.
+    res.status(200).json(thing)
+  },
+  // POST
+  createThing: async (req, res) => {
+    // Nouvel objet
+    const { title, content, comment } = req.body
+    if (!title || !content || !comment) {
+      return res.status(400).json({
+        success: false,
+        message: "Tous les champs obligatoires doivent être remplis",
+      })
+    }
+    const thing = new Thing({
+      // correspond aux éléments qui vont être crées en base.
+      title,
+      content,
+      comment,
+    })
 
-        // Nouvel objet
-        const { title, content, comment } = req.body
-        if (!title || !content || !comment ) {
-          return res.status(400).json({
-          success: false,
-          message: 'Tous les champs obligatoires doivent être remplis'
-        });
-      }
-        const thing = new Thing({
-        
-            // correspond aux éléments qui vont être crées en base.
-            title,
-            content,
-            comment,
-        })
+    try {
+      await thing.save()
+      return res.status(201).json({ message: "Objet enregistré !" })
+    } catch (error) {
+      return res.status(400).json({ error: error })
+    }
+  },
+  // PUT (by id)
+  updateThing: async (req, res) => {
+    const thing = getOneThing()
 
-        try {
-          await thing.save()
-          return res.status(201).json({ message: 'Objet enregistré !'})
-        } catch (error) {
-          return res.status(400).json({ error: error })
-        }
+    try {
+      await thing.save()
 
-}
+      return res.status(201).json({ message: "Objet modifié !" })
+    } catch (error) {
+      return res.status(400).json({ error: error })
+    }
+  },
 }
 export default thingController
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -4,6 +4,8 @@ const router = express.Router()
 import thingController from '../controllers/stuff.js'
 
 router.get("/", thingController.getThings)
+router.get("/:id", thingController.getOneThing)
 router.post("/", thingController.createThing)
+router.put("/:id", thingController.updateThing)
 
 export default router
