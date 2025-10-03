d98dcae9f1b38036432d3318555f96f9d1ff8f0b
RockmanXSigma <rockmanxsigma@gmail.com>
2025-10-03 15:44:24 +0200

    REPLACE try/catch par then/catch

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -4,25 +4,31 @@ import Thing from "../models/Thing.js"
 const thingController = {
   // GET (all)
   getThings: async (req, res) => {
-    const things = await Thing.find()
-    res.status(200).json(things)
+    await Thing.find()
+      .then((things) => res.status(200).json(things))
+      .catch((error) => res.status(400).json("Erreur : " + error))
+    // res.status(200).json(things)
   },
   // GET (by id)
   getOneThing: async (req, res) => {
-    const thing = await Thing.findById(req.params.id)
+    await Thing.findById(req.params.id)
+      .then((thing) => res.status(200).json(thing))
+      .catch((error) => res.status(400).json("Erreur : " + error))
 
-    res.status(200).json(thing)
+    // res.status(200).json(thing)
   },
   // POST
   createThing: async (req, res) => {
     // Nouvel objet
     const { title, content, comment } = req.body
+
     if (!title || !content || !comment) {
       return res.status(400).json({
         success: false,
         message: "Tous les champs obligatoires doivent être remplis",
       })
     }
+
     const thing = new Thing({
       // correspond aux éléments qui vont être crées en base.
       title,
@@ -30,18 +36,24 @@ const thingController = {
       comment,
     })
 
-    try {
-      await thing.save()
-      return res.status(201).json({ message: "Objet enregistré !" })
-    } catch (error) {
-      return res.status(400).json({ error: error })
-    }
+    await thing
+      .save()
+      .then((newDocument) => res.status(201).json(newDocument))
+      .catch((error) => res.status(400).json("Error: " + error))
+    // try {
+    //   await thing.save()
+    //   return res.status(201).json({ message: "Objet enregistré !" })
+    // } catch (error) {
+    //   return res.status(400).json({ error: error })
+    // }
   },
   // PUT (by id)
   updateThing: async (req, res) => {
-    let { title, content, comment } = req.body
+    // Récupération de l'id et des variables dans le body
     let id = req.params.id
+    let { title, content, comment } = req.body
 
+    // Mise à jour de l'objet avec gestion réponse (succès ou erreur)
     Thing.findOneAndUpdate(
       { _id: id },
       {
@@ -53,21 +65,21 @@ const thingController = {
     )
       .then((updatedThing) => res.json(updatedThing))
       .catch((err) => res.status(400).json("Error : " + err))
-    // try {
+  },
+  deleteThing: async (req, res) => {
+    // Récupération id
+    const { id } = req.params
 
-    //   return res.status(201).json({ message: "Objet modifié !" })
+    // Suppression object
+    await Thing.deleteOne({ _id: id })
+      .then((deleteResult) => res.status(200).json(deleteResult))
+      .catch((error) => res.status(400).json("Error : " + error))
+
+    // try {
+    //   return res.status(201).json({ message: "L'objet a bien été supprimé." })
     // } catch (error) {
     //   return res.status(400).json({ error: error })
     // }
-  },
-  deleteThing: async (req, res) => {
-    await Thing.deleteOne(req.body.id)
-
-    try {
-      return res.status(201).json({ message: "L'objet a bien été supprimé." })
-    } catch (error) {
-      return res.status(400).json({ error: error })
-    }
   },
 }
 export default thingController
